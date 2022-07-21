b04d683136f8db5ea52db7671b78495db0692985
sanjana8838 <sanjana8838@gmail.com>
2022-07-21 11:32:19 +0800

    Add log messages

--- modbustcplogger.py
+++ modbustcplogger.py
@@ -2,7 +2,6 @@ from pymodbus.client.sync import ModbusTcpClient
 import requests
 import time
 import datetime
-from random import randint
 import logging
 import csv
 from os.path import exists as file_exists
@@ -50,11 +49,11 @@ ploads = {
     "plc_alarm_status_1":0
 }
 
-#logging format and level
+#root logging format and level
 FORMAT = ( '%(asctime)s %(name)s - %(levelname)s - %(message)s' )
 logging.basicConfig(filename='app.log', filemode='w', format=FORMAT, level=logging.DEBUG)
 
-#logging format and level
+#non root logging format and level
 def setup_logger(logger_name, log_file, level=logging.INFO, format='%(message)s'):
     l = logging.getLogger(logger_name)
     formatter = logging.Formatter(format)
@@ -69,50 +68,51 @@ def setup_logger(logger_name, log_file, level=logging.INFO, format='%(message)s'
 #modbus connection
 def ModbusConnect():
     global client 
+    logger1.debug('Connecting to Modbus...')
     client = ModbusTcpClient('192.168.1.36', port=502)
     connect = client.connect()
     logging.debug(connect)
+    logger1.debug('Modbus Connection Status: %s', connect)
 
 #reading registers
 def ReadRegister():
     logging.debug('Reading Registers')
     logger1.debug('Reading Registers')
     ploads['timestamp'] = str(datetime.datetime.now())
-    ploads["primary_charging_relay"] = float((client.read_holding_registers(0, 2)).registers[1])
-    ploads["primary_discharge_relay"] = float(client.read_holding_registers(2, 2).registers[1])    
-    ploads["primary_positive_pump"] = float(client.read_holding_registers(4, 2).registers[1]) 
-    ploads["primary_negative_pump"]= float(client.read_holding_registers(6, 2).registers[1]) 
-    ploads["system_mode"] = float(client.read_holding_registers(8, 2).registers[1]) 
-    ploads["system_alarm_status"] = float(client.read_holding_registers(10, 2).registers[1]) 
-    ploads["balancing_valve"] = float(client.read_holding_registers(12, 2).registers[1]) 
-    ploads["positive_valve"] = float(client.read_holding_registers(14, 2).registers[1]) 
-    ploads["negative_valve"] = float(client.read_holding_registers(16, 2).registers[1]) 
-    ploads["state_of_charge"] = float(client.read_holding_registers(18, 2).registers[1]) 
-    ploads["bcu_mode_status"] = float(client.read_holding_registers(20, 2).registers[1]) 
-    ploads["bcu_voltage"] = float(client.read_holding_registers(22, 2).registers[1])
-    ploads["bcu_current"] = float(client.read_holding_registers(24, 2).registers[1]) 
-    ploads["bcu_power"] = float(client.read_holding_registers(26, 2).registers[1]) 
-    ploads["bcu_state_of_charge"] = float(client.read_holding_registers(28, 2).registers[1])
-    ploads["bcu_hydrogen_sensor"] = float(client.read_holding_registers(30, 2).registers[1]) 
-    ploads["bcu_leakage_sensor"] = float(client.read_holding_registers(32, 2).registers[1]) 
-    ploads["smoke_sensor"] = float(client.read_holding_registers(34, 2).registers[1]) 
-    ploads["bcu_ocv"] = float(client.read_holding_registers(36, 2).registers[1]) 
-    ploads["bcu_positive_tank_temp"] = float(client.read_holding_registers(38, 2).registers[1]) 
-    ploads["bcu_negative_tank_temp"] = float(client.read_holding_registers(40, 2).registers[1]) 
-    ploads["positive_tank_high_level_float"] = float(client.read_holding_registers(42, 2).registers[1]) 
-    ploads["negative_tank_high_level_float"] = float(client.read_holding_registers(44, 2).registers[1]) 
-    ploads["positive_tank_low_level_float"] = float(client.read_holding_registers(48, 2).registers[1]) 
-    ploads["positive_tank_low_level_float"] = float(client.read_holding_registers(46, 2).registers[1]) 
-    ploads["positive_tank_low_level_float"] = float(client.read_holding_registers(48, 2).registers[1]) 
-    ploads["primary_stack_voltage"] = float(client.read_holding_registers(50, 2).registers[1]) 
-    ploads["primary_stack_current"] = float(client.read_holding_registers(52, 2).registers[1]) 
-    ploads["primary_stack_positive_pressure_sensor"] = float(client.read_holding_registers(54, 2).registers[1])
-    ploads["primary_stack_negative_pressure_sensor"] = float(client.read_holding_registers(56, 2).registers[1]) 
-    ploads["positive_stack_pressure_delta"] = float(client.read_holding_registers(58, 2).registers[1]) 
-    ploads["b1_primary_stack_pressure_delta"] = float(client.read_holding_registers(60, 2).registers[1]) 
-    ploads["sensor_temp"] = float(client.read_holding_registers(62, 2).registers[1]) 
-    ploads["humidity"] = float(client.read_holding_registers(64, 2).registers[1]) 
-    logger1.debug('Reading Complete')
+    ploads["dc_main_contactor"] = float((client.read_holding_registers(0, 2, unit=2)).registers[1])
+    ploads["stk1_contactor"] = float(client.read_holding_registers(2, 2, unit=2).registers[1])    
+    ploads["primary_positive_pump"] = float(client.read_holding_registers(4, 2, unit=2).registers[1]) 
+    ploads["primary_negative_pump"]= float(client.read_holding_registers(6, 2, unit=2).registers[1]) 
+    ploads["balancing_valve"] = float(client.read_holding_registers(12, 2, unit=2).registers[1]) 
+    ploads["positive_valve"] = float(client.read_holding_registers(14, 2, unit=2).registers[1]) 
+    ploads["negative_valve"] = float(client.read_holding_registers(16, 2, unit=2).registers[1]) 
+    ploads["state_of_charge"] = float(client.read_holding_registers(18, 2, unit=2).registers[1]) 
+    ploads["bcu_mode_status"] = float(client.read_holding_registers(20, 2, unit=2).registers[1]) 
+    ploads["bcu_voltage"] = float(client.read_holding_registers(22, 2, unit=2).registers[1])
+    ploads["bcu_current"] = float(client.read_holding_registers(24, 2, unit=2).registers[1]) 
+    ploads["bcu_power"] = float(client.read_holding_registers(26, 2, unit=2).registers[1]) 
+    ploads["bcu_state_of_charge"] = float(client.read_holding_registers(28, 2, unit=2).registers[1])
+    ploads["bcu_hydrogen_sensor"] = float(client.read_holding_registers(30, 2, unit=2).registers[1]) 
+    ploads["bcu_leakage_sensor"] = float(client.read_holding_registers(32, 2, unit=2).registers[1]) 
+    ploads["smoke_sensor"] = float(client.read_holding_registers(34, 2, unit=2).registers[1]) 
+    ploads["bcu_ocv"] = float(client.read_holding_registers(36, 2, unit=2).registers[1]) 
+    ploads["bcu_positive_tank_temp"] = float(client.read_holding_registers(38, 2, unit=2).registers[1]) 
+    ploads["bcu_negative_tank_temp"] = float(client.read_holding_registers(40, 2, unit=2).registers[1]) 
+    ploads["positive_tank_high_level_float"] = float(client.read_holding_registers(42, 2, unit=2).registers[1]) 
+    ploads["negative_tank_high_level_float"] = float(client.read_holding_registers(44, 2, unit=2).registers[1]) 
+    ploads["positive_tank_low_level_float"] = float(client.read_holding_registers(46, 2, unit=2).registers[1]) 
+    ploads["negative_tank_low_level_float"] = float(client.read_holding_registers(48, 2, unit=2).registers[1]) 
+    ploads["primary_stack_voltage"] = float(client.read_holding_registers(50, 2, unit=2).registers[1]) 
+    ploads["primary_stack_current"] = float(client.read_holding_registers(52, 2, unit=2).registers[1]) 
+    ploads["primary_stack_positive_pressure_sensor"] = float(client.read_holding_registers(54, 2, unit=2).registers[1])
+    ploads["primary_stack_negative_pressure_sensor"] = float(client.read_holding_registers(56, 2, unit=2).registers[1]) 
+    ploads["positive_stack_pressure_delta"] = float(client.read_holding_registers(58, 2, unit=2).registers[1]) 
+    ploads["b1_primary_stack_pressure_delta"] = float(client.read_holding_registers(60, 2, unit=2).registers[1]) 
+    ploads["sensor_temp"] = float(client.read_holding_registers(64, 2, unit=2).registers[1]) 
+    ploads["humidity"] = float(client.read_holding_registers(62, 2, unit=2).registers[1]) 
+#    ploads["bms_timestamp"] = float(client.read_holding_registers(102, 2, unit=2).registers[1]) 
+#    ploads["plc_alarm_status_1"] = float(client.read_holding_registers(104, 2, unit=2).registers[1]) 
+    logger1.debug('Reading Register Complete')
     csv_w(ploads)
  
 def SendData():
@@ -120,6 +120,7 @@ def SendData():
     t = http_client.post(url, json=ploads)
     logging.debug((str(t.text)))
     logger1.debug(ploads)
+    logger1.debug('POST Complete')
 
 def PrintData():
     print(ploads)
@@ -136,9 +137,9 @@ if __name__ == "__main__":
     global logger2
     global logger1
     setup_logger('log1', 'run.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
-    setup_logger('log2', 'parameter.log')  
+    #setup_logger('log2', 'parameter.log')  
     logger1 = logging.getLogger('log1')
-    logger2 = logging.getLogger('log2')  
+    #logger2 = logging.getLogger('log2')  
 
     #CSV Setup
     if not file_exists('Parameters.csv'):
